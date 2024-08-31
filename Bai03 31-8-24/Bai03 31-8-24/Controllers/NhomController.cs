4feef2fb45c82e5a7f025a473cbd3dc072821c3e
Dyan1210 <pphantruong1234@gmail.com>
2024-08-31 14:25:22 +0700

    Da xong

---
+++ Bai03 31-8-24/Bai03 31-8-24/Controllers/NhomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bai03_31_8_24.Controllers
+{
+    public class NhomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bai03 31-8-24/Bai03 31-8-24/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bai03_31_8_24.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
